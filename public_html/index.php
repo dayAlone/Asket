11671132e8a4a4c52ab7198a784339577a983b40
dayAlone <andrey.slider@gmail.com>
2014-10-13 15:43:16 +0600

    Index

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,84 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Аскет-Авто');
+?>
+<section class="promo">
+  <div class="row">
+    <div class="col-md-3 col-xs-3">
+      <a href="#" data-id="#new" class="promo__banner">
+        <div class="promo__banner-title">Новая<br>техника</div>
+      </a>
+      <a href="#" data-id="#used" class="promo__banner">
+        <div class="promo__banner-title">Б/У <br>техника</div>
+        <div class="promo__banner-description">С возможностью <br>рассрочки</div>
+      </a>
+    </div>
+    <div class="col-md-9 col-xs-9">
+      <div style="background-image: url(./layout/images/slider-bg.jpg)" class="resp_slider">
+        <div class="content">
+          <h1>Специальное предложение <br>на автокраны “Ивановец” <br>25 тонн. Скидки!</h1><a class="button">Подробнее</a>
+        </div>
+      </div>
+    </div>
+    <?
+    $APPLICATION->IncludeComponent("bitrix:catalog.section.list", "index", array(
+        "IBLOCK_TYPE"  => "content",
+        "IBLOCK_ID"    => "1",
+        "SECTION_ID"   => "",
+        "SECTION_CODE" => "",
+        "TOP_DEPTH"    => "2",
+        "CACHE_TYPE"   => "A",
+        "CACHE_TIME"   => "3600",
+    ),
+    false
+    );
+  ?>
+    <div id="used" class="promo-slide">
+      <div class="row">
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-5.png)" class="promo-slide-item_image"></div>
+            <div class="promo-slide-item_title"><span>Седельные тягачи</span></div></a></div>
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-6.png)" class="promo-slide-item_image"></div>
+            <div class="promo-slide-item_title"><span>Самосвалы</span></div></a></div>
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-7.png)" class="promo-slide-item_image"></div>
+            <div class="promo-slide-item_title"><span>Бортовые грузовики, фургоны</span></div></a></div>
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-8.png)" class="promo-slide-item_image"></div>
+            <div class="promo-slide-item_title"><span>Прицепы, полуприцепы</span></div></a></div>
+      </div>
+      <div class="row">
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-9.png)" class="promo-slide-item_image"></div>
+            <div class="promo-slide-item_title"><span>Дорожно-строительная спецтехника</span></div></a></div>
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-10.png)" class="promo-slide-item_image"></div>
+            <div class="promo-slide-item_title"><span>Мусоровозы, бункеровозы</span></div></a></div>
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-11.png)" class="promo-slide-item_image"></div>
+            <div class="promo-slide-item_title"><span>Краны-манипуляторы</span></div></a></div>
+      </div>
+    </div>
+    <div id="new" class="promo-slide">
+      <div class="row">
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-1.png)" class="promo-slide-item_image promo-slide-item_image--height"></div>
+            <div class="promo-slide-item_title"><span>Автокраны</span></div></a></div>
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-2.png)" class="promo-slide-item_image"></div>
+            <div class="promo-slide-item_title"><span>Мусоровозы, бункеровозы</span></div></a></div>
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-3.png)" class="promo-slide-item_image promo-slide-item_image--height"></div>
+            <div class="promo-slide-item_title"><span>Краны-манипуляторы</span></div></a></div>
+        <div class="col-md-3 col-xs-3"><a class="promo-slide-item">
+            <div style="background-image: url(./layout/images/c-4.png)" class="promo-slide-item_image"></div>
+            <div class="promo-slide-item_title"><span>Легкие коммерческие автомобили</span></div></a></div>
+      </div>
+    </div>
+  </div>
+</section>
+<?
 $APPLICATION->IncludeComponent("bitrix:news.list", "tabs_index", 
 array(
   "IBLOCK_ID"   => 6,
