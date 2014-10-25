f5288a233eb1ade9f3b916209e51d1281052dd50
dayAlone <andrey.slider@gmail.com>
2014-10-25 17:08:05 +0600

    Nav

--- public_html/local/templates/.default/components/bitrix/catalog.section.list/nav/template.php
+++ public_html/local/templates/.default/components/bitrix/catalog.section.list/nav/template.php
@@ -2,11 +2,14 @@
 <?
   $arSections = array();
   $second = false;
+  $length = 0;
   foreach ($arResult['SECTIONS'] as $key => $item):
     $array = array('NAME'=>$item['NAME'], 'URL'=>$item['SECTION_PAGE_URL'], "CODE"=>$item['CODE'], 'ID' => $item['ID']);
     if(!isset($arSections[$item['DEPTH_LEVEL']]))
       $arSections[$item['DEPTH_LEVEL']] = array();
     if($item['DEPTH_LEVEL']==2) {
+      if($arParams['SECTIONS_LIST'][1]==$item['CODE'])
+        $length = strlen($item['NAME']);
       if(!isset($arSections[$item['DEPTH_LEVEL']]))
         $arSections[$item['DEPTH_LEVEL']][$item['IBLOCK_SECTION_ID']] = array();
       $arSections[$item['DEPTH_LEVEL']][$item['IBLOCK_SECTION_ID']][] = $array;
@@ -28,12 +31,12 @@
       <?endforeach;?>
     </select>
   </div>
-  <div class="col-xs-4">
+  <div class="col-xs-<?=($length>=30?"4":"3")?>">
     <select>
       <?foreach ($arSections[2][$second] as $item):?>
         <option <?=($arParams['SECTIONS_LIST'][1]==$item['CODE']?"selected":"")?> value="<?=$item['URL']?>"><?=$item['NAME']?></option>
       <?endforeach;?>
     </select>
   </div>
-  <div class="col-xs-6"><?=$arParams['NAME']?></div>
+  <div class="col-xs-<?=($length>=30?"7":"8")?>"><?=$arParams['NAME']?></div>
 </div>
