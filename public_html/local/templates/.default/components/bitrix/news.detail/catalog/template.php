24b8829b4d4962d4b026277d32fcf75cb6ceeab0
dayAlone <andrey.slider@gmail.com>
2014-11-10 02:10:06 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.detail/catalog/template.php
+++ public_html/local/templates/.default/components/bitrix/news.detail/catalog/template.php
@@ -15,7 +15,9 @@ $props = &$item["PROPS"];
         <div class="gallery">
             <div class="big">
                 <?foreach ($props['PHOTOS'] as $key => $value):?>
-                <a id="big-<?=$key?>" style="background-image: url(<?=$value['small']?>)" rel="prettyPhoto[]" href="<?=$value['value']?>" <?=($key==0?'class="active"':'')?>></a>
+                <a id="big-<?=$key?>" style="background-image: url(<?=$value['small']?>)" rel="prettyPhoto[]" href="<?=$value['value']?>" <?=($key==0?'class="active"':'')?>>
+                  <img src="<?=$value['small']?>" alt="">
+                </a>
                 <?endforeach;?>
             </div>
             
