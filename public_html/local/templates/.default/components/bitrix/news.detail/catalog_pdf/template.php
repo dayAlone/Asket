e632598e11f44c8fdea620bcea85a447466a9205
dayAlone <andrey.slider@gmail.com>
2014-11-10 20:59:27 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.detail/catalog_pdf/template.php
+++ public_html/local/templates/.default/components/bitrix/news.detail/catalog_pdf/template.php
@@ -4,88 +4,109 @@ $item = $arResult;
 $props = &$item["PROPS"];
 ?>
 <div class="product">
-  <h1><?=$item['NAME']?></h1>
-  <table cellpadding="10">
-    <tr>
-      <td width="50%">
-        <div class="big">
-            <?foreach ($props['PHOTOS'] as $key => $value):?>
-              <img src="<?=$value['small']?>" alt="">
+  <div class="row <?=(isset($_REQUEST['pdf'])?"no-gutter":"")?>">
+    <div class="<? if(!isset($_REQUEST['pdf'])):?>col-xs-10<?else:?>col-xs-12<?endif;?>">
+      <h1><?=$item['NAME']?></h1>
+    </div>
+    <? if(!isset($_REQUEST['pdf'])):?>
+    <div class="col-xs-2"><a href="#" class="print"><?=svg('print')?>Версия для печати</a></div>
+    <?endif;?>
+  </div>
+  <div class="row <?=(isset($_REQUEST['pdf'])?"no-gutter":"")?>">
+    <div class="col-xs-4 side">
+        <div class="gallery">
+            <div class="big">
+                <?foreach ($props['PHOTOS'] as $key => $value):?>
+                <a id="big-<?=$key?>" style="background-image: url(<?=$value['small']?>)" rel="prettyPhoto[]" href="<?=$value['value']?>" <?=($key==0?'class="active"':'')?>>
+                  <img src="<?=$value['small']?>" alt="">
+                </a>
+                <?endforeach;?>
+            </div>
+            
+            <div class="slider">
+                <? if(count($props['PHOTOS'])>1):?>
+                <?foreach ($props['PHOTOS'] as $key => $value):?>
+                    <div class="item"><a style="background-image: url(<?=$value['small']?>)" data-id="big-<?=$key?>" class="image"></a></div>
+                <?endforeach;?>
+                <?endif;?>
+            </div>
+        </div>
+        <p>
+          <?=$item['PREVIEW_TEXT']?>
+        </p>
+    </div>
+    <div class="col-xs-8">
+        <div class="params">
+            <?
+              $list = array(
+                "YEAR"         => array("icon"=>1, "name"=> "Год выпуска"),
+                "STATUS"       => array("icon"=>2, "name"=> "Состояние"),
+                "WORK"         => array("icon"=>3, "name"=> "Наработка"),
+                "PLACE"        => array("icon"=>4, "name"=> "Нахождение"),
+                "AVAILABILITY" => array("icon"=>5, "name"=> "Наличие")
+              );
+              $i=1;
+            ?>
+            <?foreach ($list as $key => $value):?>
+              <?if(isset($props[$key])):?>
+                <div class="params-item">
+                  <div class="params-item-title">
+                    <div class="params-item-title_content"><?=svg('i-'.$value["icon"])?><br><?=$value["name"]?></div>
+                  </div>
+                  <div class="params-item-value"><?=$props[$key]?></div>
+                </div>
+              <?endif;?>
             <?endforeach;?>
         </div>
-      </td>
-      <td width="50%" valign="top">
-        
-        
-          <?
-            $list = array(
-              "YEAR"         => array("icon"=>1, "name"=> "Год выпуска", "width"=>17, "height"=>17),
-              "STATUS"       => array("icon"=>2, "name"=> "Состояние",   "width"=>17, "height"=>17),
-              "WORK"         => array("icon"=>3, "name"=> "Наработка",   "width"=>17, "height"=>17),
-              "PLACE"        => array("icon"=>4, "name"=> "Нахождение",  "width"=>17, "height"=>17),
-              "AVAILABILITY" => array("icon"=>5, "name"=> "Наличие",     "width"=>17, "height"=>17)
-            );
-            $i=1;
-          ?>
-          <table cellpadding="5">
-          <?foreach ($list as $key => $value):?>
-            <?if(isset($props[$key])):?>
-              <tr>
-                <td width="30"><img src="/layout/images/svg/i-<?=$value["icon"]?>.svg" width="<?=$value["width"]?>" height="<?=$value["height"]?>"></td>
-                <td class="title"><?=$value["name"]?></td>
-                <td><?=$props[$key]?></td>
-              </tr>
-            <?endif;?>
-          <?endforeach;?>
-          </table>
         <div class="price">
         <?
         if($props['PRICE_ORDER']!='Y'):
-        if($props['PRICE_SALE']):?>
-          <div class="price-title">Новая цена по акции</div>
-          <div class="price-value"><?=number_format($props['PRICE_SALE'], 0, '.', ' ')?> р.</div>
-          <div class="price-old"><?=number_format($props['PRICE'], 0, '.', ' ')?> р.</div>
-        <?else:?>
-            <div class="price-value">
-              <?if(intval($props['PRICE'])>0):?>
-                <?=number_format($props['PRICE'], 0, '.', ' ')?> р.
-              <?
-              endif;?>
-            </div>
-        <?endif;
+          if($props['PRICE_SALE']):?>
+            <div class="price-title">Новая цена по акции</div>
+            <div class="price-value"><?=number_format($props['PRICE_SALE'], 0, '.', ' ')?> р.</div>
+            <div class="price-old"><?=number_format($props['PRICE'], 0, '.', ' ')?> р.</div>
+          <?else:?>
+              <div class="price-value">
+                <?if(intval($props['PRICE'])>0):?>
+                  <?=number_format($props['PRICE'], 0, '.', ' ')?> р.
+                <?
+                endif;?>
+              </div>
+          <?endif;
         else:
-        ?><div class="price-value">По запросу</div><?
+          ?><div class="price-value">По запросу</div><?
         endif;?>
+            <a href="/leasing/" class="price-button">Выгодно в лизинг</a>
         </div>
-      </td>
-    </tr>
-  </table>
-  <table cellpadding="10">
-    <?
-      $title = false;
-      if(count($props['BODY'])>0):?>
-        <?foreach ($props['BODY'] as $key=>$item):?>
-          <?if($item['property_title']=="Y"):
-            if(!$title) $title = true;
-            ?>
-            <tr>
-              <td colspan="2"><?=$item['property_name']?></td>
-            </tr>
-            <?
-          else:?>
-            <tr>
-              <td width="180"><?=$item['property_name']?>:</td>
-              <td width=""><?=$item['property_value']?></td>
-            </tr>
-          <?
-          endif;
-          endforeach;
-      endif;
-      ?>
-  </table>
+        <div class="sub-tabs">
+            <div class="sub-tabs_title">
+              <a href="#params" class="sub-tabs_title__active">Характеристики</a>
+              <?if(strlen($props['DEPRECIATION']['TEXT'])>0):?><a href="#depreciation">Износ</a><?endif;?>
+            </div>
             <div class="sub-tabs_content sub-tabs_content--active" id="params">
               
-            
+            <?
+            $title = false;
+            if(count($props['BODY'])>0):?>
+              <?foreach ($props['BODY'] as $key=>$item):?>
+                <?if($item['property_title']=="Y"):
+                  if(!$title) $title = true;
+                  if($key!=0) echo "</div>";
+                  ?>
+                  <div class="param-block">
+                    <div class="param-block_title"><?=$item['property_name']?></div>
+                  <?
+                else:?>
+                  <div class="row">
+                    <div class="col-md-5 col-xs-5"><?=$item['property_name']?>:</div>
+                    <div class="col-md-7 col-xs-7"><?=$item['property_value']?></div>
+                  </div>
+                <?
+                endif;
+                endforeach;
+                if($title) echo "</div>";
+            endif;
+            ?>
             
               
 
@@ -107,3 +128,31 @@ $props = &$item["PROPS"];
             </div>
             <?endif;?>
           </div>
+          <? if(!isset($_REQUEST['pdf'])):?>
+          <?
+            $rsPath = GetIBlockSectionPath($arResult['IBLOCK_ID'], $arResult['IBLOCK_SECTION_ID']);
+            $arSections = array();
+            while($arPath = $rsPath->GetNext())
+              $arSections[] = $arPath['CODE'];
+          ?>
+          <a href="<?=SITE_URL?>/catalog/<?=end($arSections)?>/" class="news-list_item-back"><img src="/layout/images/back.png">Вернуться в раздел</a>
+          <?endif;?>
+    </div>
+  </div>
+</div>
+<?$this->SetViewTarget('nav');
+$APPLICATION->IncludeComponent("bitrix:catalog.section.list", "nav", array(
+    "IBLOCK_TYPE"  => "content",
+    "IBLOCK_ID"    => "1",
+    "SECTION_ID"   => "",
+    "SECTION_CODE" => "",
+    "TOP_DEPTH"    => "2",
+    "CACHE_TYPE"   => "A",
+    "CACHE_TIME"   => "36000",
+    "NAME"         => $arResult['NAME'],
+    "SECTIONS_LIST"=> json_encode($arSections),
+    "CACHE_NOTES"  => $arParams['PARENT_SECTION']
+),
+false
+);
+$this->EndViewTarget();?> 
--- public_html/local/templates/pdf/header.php
+++ public_html/local/templates/pdf/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html><html lang='ru'>
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="/layout/css/print.css">
+  <link rel="stylesheet" href="/layout/css/frontend.css">
 </head>
 <body>
 <div class="container">
