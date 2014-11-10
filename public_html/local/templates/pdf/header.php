351a6c6ca08dea7bf7e38f403639ef6c10d6a30b
dayAlone <andrey.slider@gmail.com>
2014-11-10 21:45:36 +0500

    Header

--- public_html/local/templates/pdf/header.php
+++ public_html/local/templates/pdf/header.php
@@ -5,5 +5,22 @@
 </head>
 <body class="print">
   <div class="container">
-
+  <?
+    function full_path()
+    {
+        $s = &$_SERVER;
+        $ssl = (!empty($s['HTTPS']) && $s['HTTPS'] == 'on') ? true:false;
+        $sp = strtolower($s['SERVER_PROTOCOL']);
+        $protocol = substr($sp, 0, strpos($sp, '/')) . (($ssl) ? 's' : '');
+        $port = $s['SERVER_PORT'];
+        $port = ((!$ssl && $port=='80') || ($ssl && $port=='443')) ? '' : ':'.$port;
+        $host = isset($s['HTTP_X_FORWARDED_HOST']) ? $s['HTTP_X_FORWARDED_HOST'] : (isset($s['HTTP_HOST']) ? $s['HTTP_HOST'] : null);
+        $host = isset($host) ? $host : $s['SERVER_NAME'] . $port;
+        $uri = $protocol . '://' . $host . $s['REQUEST_URI'];
+        $segments = explode('?', $uri, 2);
+        $url = $segments[0];
+        return $url;
+    }
+  ?>
+  Ссылка на объявление: <?=full_path()?>
   <hr>
