9b51f62cdcbdb289af89fe3d98ea2484bbc5ad58
dayAlone <andrey.slider@gmail.com>
2014-11-10 20:40:31 +0500

    Changes

--- public_html/1.php
+++ public_html/1.php
@@ -3,7 +3,7 @@ error_reporting(0);
 ini_set('xdebug.max_nesting_level', 20000); 
 set_time_limit(0);
 include($_SERVER['DOCUMENT_ROOT']."/include/pdf/mpdf.php");
-$html = file_get_contents('http://asket.radia.ru/catalog/avtokran-ivanovets-ks-45717k-3r-ovoid/?pdf=1');
+$html = file_get_contents('http://asket.radia.ru/catalog/avtokran-ivanovets-ks-45717k-3r-ovoid/?pdf=1&clear_cache=Y');
 
 $mpdf=new mPDF(); 
 $mpdf->WriteHTML($html);
