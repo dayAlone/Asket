d23fc1eaf2f0bb7b8b7cf9ff89735b642bc2a483
dayAlone <andrey.slider@gmail.com>
2014-11-10 22:00:31 +0500

    CSS

--- public_html/local/templates/pdf/footer.php
+++ public_html/local/templates/pdf/footer.php
@@ -1,3 +1,17 @@
+<div class="pdf-footer">
+	<div class="row">
+		<div class="col-xs-8">
+			<strong>Телефоны</strong>: +7 (495) 781-50-78, +7 (915) 071-17-74 <br>
+			<strong>E-mail</strong>: info@asket-auto.ru <br>
+			<strong>Наш адрес</strong>: 117105, г. Москва, ул. Нагатинская, д. 1, стр. 1
+		</div>
+		<div class="col-xs-4 right">
+			<img src="/layout/images/" alt="">
+		</div>
+	</div>
 </div>
+
+</div>
+
 </body>
 </html>
