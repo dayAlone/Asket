b8cfcb3da46bf73552d3372d183d8341665d3a5d
dayAlone <andrey.slider@gmail.com>
2014-11-14 23:12:20 +0500

    Header

--- public_html/include/header.php
+++ public_html/include/header.php
@@ -20,6 +20,9 @@
   <?
     $APPLICATION->ShowHead();
   ?>
+  <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <![endif]-->
 </head>
 <body class="<?=$APPLICATION->AddBufferContent("body_class");?>">
 <div id="panel"><?$APPLICATION->ShowPanel();?></div>
